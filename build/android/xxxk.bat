be3bacd940668cbc0908f7f0b1b63f2b5ed0c3ab
thxnder <tswwe@qq.com>
2026-05-05 09:22:41 +0800

    * dict: 更新键道方案码表，与 RIME 键道同步（20260430）。 * dict: “在线码表”表述调整为“反馈码表修改建议”（Windows 版的菜单 menu.ini 同步修改）。 * dict: 假名（Kana）由 or 改为 ok 触发；日期时间（time）由 oo 改为 ot 触发。 * chore: 调整 xxxk.bat 编译逻辑，将码表等主要数据从 assets 转移至 assets/.yong（version.txt 和 yong.ini 仍留在 assets），皮肤文件从 assets/www 转移至 assets/.yong/android，默认不打包 english.txt 以节约内存。

--- build/android/xxxk.bat
+++ build/android/xxxk.bat
@@ -33,21 +33,21 @@ rem home\skin\xxxktray2.png -> yong-android\res\drawable-*dpi\app_icon.png -> AP
 copy %dirHome%\skin\xxxktray2.png %dirData%\res\drawable-hdpi\app_icon.png /y
 copy %dirHome%\skin\xxxktray2.png %dirData%\res\drawable-mdpi\app_icon.png /y
 copy %dirHome%\skin\xxxktray2.png %dirData%\res\drawable-xhdpi\app_icon.png /y
-rem home\skin-android -> yong-android\assets\www -> APP SKIN
-xcopy %dirHome%\skin-android %dirData%\assets\www /i /s /e /y
-rem home\mb -> yong-android\assets\mb -> sdcard\yong\mb
+rem home\skin-android -> yong-android\assets\.yong\android -> APP SKIN
+xcopy %dirHome%\skin-android %dirData%\assets\.yong\android /i /s /e /y
+rem home\mb -> yong-android\assets\.yong\mb -> sdcard\yong\.yong\mb
 rmdir /s /q %dirData%\assets\mb
-xcopy %dirHome%\mb\xkbase %dirData%\assets\mb\xkbase /i /s /e /y
-xcopy %dirHome%\mb\xkjd6 %dirData%\assets\mb\xkjd6 /i /s /e /y
-xcopy %dirHome%\mb\xklb %dirData%\assets\mb\xklb /i /s /e /y
-xcopy %dirHome%\mb\xkxb %dirData%\assets\mb\xkxb /i /s /e /y
-xcopy %dirHome%\mb\xkyb %dirData%\assets\mb\xkyb /i /s /e /y
-copy %dirHome%\english.txt %dirData%\assets\mb\english.txt /y
-rem home -> yong-android\assets -> sdcard\yong
-copy %dirHome%\bd.txt %dirData%\assets\bd.txt /y
-copy %dirHome%\bihua.bin %dirData%\assets\bihua.bin /y
-copy %dirHome%\crab.txt %dirData%\assets\crab.txt /y
-copy %dirHome%\urls.txt %dirData%\assets\urls.txt /y
+xcopy %dirHome%\mb\xkbase %dirData%\assets\.yong\mb\xkbase /i /s /e /y
+xcopy %dirHome%\mb\xkjd6 %dirData%\assets\.yong\mb\xkjd6 /i /s /e /y
+xcopy %dirHome%\mb\xklb %dirData%\assets\.yong\mb\xklb /i /s /e /y
+xcopy %dirHome%\mb\xkxb %dirData%\assets\.yong\mb\xkxb /i /s /e /y
+xcopy %dirHome%\mb\xkyb %dirData%\assets\.yong\mb\xkyb /i /s /e /y
+rem copy %dirHome%\mb\english.txt %dirData%\assets\.yong\mb\english.txt /y
+rem home -> yong-android\assets\.yong -> sdcard\yong\.yong  或 sdcard\yong
+copy %dirHome%\bd.txt %dirData%\assets\.yong\bd.txt /y
+copy %dirHome%\bihua.bin %dirData%\assets\.yong\bihua.bin /y
+copy %dirHome%\crab.txt %dirData%\assets\.yong\crab.txt /y
+copy %dirHome%\urls.txt %dirData%\assets\.yong\urls.txt /y
 copy %dirHome%\version.txt %dirData%\assets\version.txt /y
 copy %dirHome%\yong-android.ini %dirData%\assets\yong.ini /y
 if %result%==2 (pause && goto menu)
